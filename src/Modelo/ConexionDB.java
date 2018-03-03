425b7229c06aa9197161917cf89f8d08a4e24e9a
Stevenpach10 <Stevenpach10@gmail.com>
2018-03-03 00:35:52 -0600

    Commit de Conexion
    aasasdasd

--- src/Modelo/ConexionDB.java
+++ src/Modelo/ConexionDB.java
@@ -1,36 +1,4 @@
 package Modelo;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import javax.swing.JOptionPane;
-import sun.jdbc.odbc.JdbcOdbcDriver;
-
-
-
 public class ConexionDB {
 
-    private static Connection Con;
-
-    public static void OpenConnection(){
-        try{
-            DriverManager.registerDriver(new sun.jdbc.odbc.JdbcOdbcDriver() );
-            Con = DriverManager.getConnection("jdbc:odbc:pos");
-            JOptionPane.showMessageDialog(null, "Connected");
-        }catch (Exception e){
-
-
-        }
-    }
-
-    public static void main(String arg[]){
-        OpenConnection();
-    }
-
-    public static Connection GetConnection(){
-
-        return Con;
-
-
-    }﻿
 }
