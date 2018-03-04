93dc9c880e0a60ec5bda4fffeebe59390b94b11f
alpizarjose97 <alpizarjose97@gmail.com>
2018-03-04 16:20:56 -0600

    Actualización de controlador

--- src/Controlador/PrincipalControlador.java
+++ src/Controlador/PrincipalControlador.java
@@ -3,9 +3,28 @@ package Controlador;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class PrincipalControlador {
     //Controles
     @FXML
     public Button botonCerrar;
+
+    public void obtenerAsistencia(ActionEvent event){
+
+    }
+
+    public void reportarDesglose(ActionEvent event){
+
+    }
+
+    public void reportarAbstencionismo(ActionEvent event){
+
+    }
+
+    public void reportarGanador(ActionEvent event){
+
+    }
 }
 
